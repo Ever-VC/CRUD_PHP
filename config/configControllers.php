21aba49e4e0348be52a34817b1b6e08c14ea7a03
Ever-VC <ever.vasquezc27@gmail.com>
2023-10-19 19:51:47 -0600

    Commit inicial: Archivos de configuración agregados

---
+++ config/configControllers.php
@@ -0,0 +1,5 @@
+<?php
+define ('BASE_DIR', "http://localhost/CRUD_PHP/");
+define ('CONTROLLERS_DIR', "controllers/");
+define ('DEFAULT_CONTROLLER', "Home");
+define ('DEFAULT_ACTION', "index");
---
+++ config/configDatabase.php
@@ -0,0 +1,6 @@
+<?php
+$host = "localhost";
+$port = 5432;
+$dbname = "tienda";
+$user = "postgres";
+$pass = "123";
